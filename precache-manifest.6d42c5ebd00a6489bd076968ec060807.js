270fe3b0022320e06661bd4956f6c50de9d707b8
janet <janet.han@gmail.com>
2020-10-02 14:57:26 -0400

    Updates

---
+++ precache-manifest.6d42c5ebd00a6489bd076968ec060807.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcbc4a0b2e2f29877a482227cf4b1f93",
+    "url": "/funny-buttons-in-react/index.html"
+  },
+  {
+    "revision": "7d05905210e50d185368",
+    "url": "/funny-buttons-in-react/static/css/main.0dbf51f8.chunk.css"
+  },
+  {
+    "revision": "0d18b746d8401bc84dfb",
+    "url": "/funny-buttons-in-react/static/js/2.fac57dd7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/funny-buttons-in-react/static/js/2.fac57dd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d05905210e50d185368",
+    "url": "/funny-buttons-in-react/static/js/main.8b89f882.chunk.js"
+  },
+  {
+    "revision": "0d7ce1fdabf20b89af71",
+    "url": "/funny-buttons-in-react/static/js/runtime-main.630731db.js"
+  }
+]);
